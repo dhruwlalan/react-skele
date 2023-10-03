e6e974d06baa9264515c555f69be7d7040d46d86
dhruwlalan <dhruwlalan22@gmail.com>
2023-10-04 01:38:31 +0530

    fix: exports

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vitest/config';
-
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      include: ["src"],
+      exclude: ["src/stories", "src/__tests__"],
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/Skeleton.tsx"),
+      formats: ["es", "umd"],
+      name: 'ReactSkele',
+      fileName: (format) => `main.${format}.js`,
+    },
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
 });
